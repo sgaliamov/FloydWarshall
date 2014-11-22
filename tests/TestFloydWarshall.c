e3e01a0284328d66fedcd862eca8a9781a102a4d
6alyamov <6alyamov@gmail.com>
2014-11-22 21:25:36 +0300

    Test_FloydWarshall_Build

--- tests/TestFloydWarshall.c
+++ tests/TestFloydWarshall.c
@@ -19,7 +19,6 @@ void Test_FloydWarshall_Build_Simple() {
 }
 
 void Test_FloydWarshall_Build_Simple_Infinity() {
-
     PFloydWarshallData data = fw_load_graph("tests/test-simple-infinity.grh");
     fw_build(data);
 
@@ -27,7 +26,7 @@ void Test_FloydWarshall_Build_Simple_Infinity() {
 
     TEST_ASSERT(fw_get_next(data, 0, 4) == 2);
     TEST_ASSERT(fw_get_next(data, 2, 4) == 4);
-    TEST_ASSERT(fw_get_next(data, 2, 10) == 10);
+    TEST_ASSERT(fw_get_next(data, 2, 10) == 4);
     TEST_ASSERT(fw_get_next(data, 0, 10) == UNDEFINED_GRAPH_NODE);
     TEST_ASSERT(fw_get_next(data, 0, 6) == UNDEFINED_GRAPH_NODE);
     TEST_ASSERT(fw_get_next(data, 0, 8) == UNDEFINED_GRAPH_NODE);
@@ -53,9 +52,14 @@ void Test_FloydWarshall_Build_Simple_Infinity() {
 }
 
 void Test_FloydWarshall_Build() {
-    PFloydWarshallData data = fw_load_graph("tests/test-load.grh");
+    PFloydWarshallData data = fw_load_graph("tests/test-build.grh");
     fw_build(data);
 
+    TEST_ASSERT(fw_get_dist(data, 4, 0) == 7);
+    TEST_ASSERT(fw_get_next(data, 4, 0) == 2);
+    TEST_ASSERT(fw_get_next(data, 2, 0) == 3);
+    TEST_ASSERT(fw_get_next(data, 3, 0) == 1);
+    TEST_ASSERT(fw_get_next(data, 1, 0) == 0);
 }
 
 TEST_BEGIN
